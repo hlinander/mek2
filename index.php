17a9c7773a993620d8519b8634fac46a8af510a0
Stellan Ostlund <stellan.ostlund@gmail.com>
2017-02-14 18:34:02 +0100

    Added problems for 8't edition See subdirectory 2017

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
 </tr></table>
 <br>
 <p>
-<TABLE border=0>
+<TABLE>
 <TR><TD class=c1>  </TD><TD class=c2>  
 </TD></TR>
 </TABLE>
@@ -67,7 +67,8 @@ Nedan finns ett <em> inofficiellt schema </em>
 
 
 
-<P CLASS="pagebreak"> <TABLE style="border=1; width=100%" > <TR> <TD colspan=4 bgcolor=LightBlue > Vecka 12  Kapitel 5 - Beskrivning av rörelse i två dimensioner </TD></TR>
+<TABLE border=1>
+<TR> <TD colspan=4 bgcolor=LightBlue > Vecka 12  Kapitel 5 - Beskrivning av rörelse i två dimensioner </TD></TR>
 <TR> <TD class=c1> 2017-03-21 </TD><TD class=c2> FÖ 10:00  </TD><TD class=c3><A HREF="FL-01">5.1,5.2, 5.3  </A>  </TD> <TD class=c4> 
 	uppgift 5.30, (se 5.36 ), 5.51
 	</TD> </TR>
@@ -84,7 +85,7 @@ Nedan finns ett <em> inofficiellt schema </em>
 <TR> <TD class=c1> 2017-03-22 </TD><TD class=c2> RÖ 10:00 </TD><TD class=c3><A HREF="LRO.html"></A> </TD><TD>
 RÖ 5.8
 </TD> </TR>
-<P CLASS="pagebreak"> <TABLE border=1 width=90%> <TR> <TD colspan=4 bgcolor=LightBlue > Vecka 13  - Kapitel 6 - 2 dimensionell kinematik </TD></TR>
+<TR> <TD colspan=4 bgcolor=LightBlue > Vecka 13  - Kapitel 6 - 2 dimensionell kinematik </TD></TR>
 <TR> <TD class=c1> 2017-03-28 </TD><TD class=c2> RÖ 10:00   </TD><TD class=c3><A HREF="LRO.html"></A> </TD><TD>
 RÖ  - Kapitel 5
 </TD> </TR>
@@ -106,7 +107,7 @@ Rörelsemängd och Rörelsemängdsmoment: 6.8,6.9 </A>
  </TD> <TD class=c4> 
 
 </TD> </TR>
-<P CLASS="pagebreak"> <TABLE border=1 width=90%> <TR> <TD colspan=4 bgcolor=LightBlue > Vecka 14  - Kapitel 7 - 3D rotationer </TD></TR>
+<TR> <TD colspan=4 bgcolor=LightBlue > Vecka 14  - Kapitel 7 - 3D rotationer </TD></TR>
 <TR> <TD class=c1> 2017-04-04 </TD><TD class=c2> FÖ 10:00   </TD><TD class=c3><A HREF="FL-08"></A>  
 7.1,7.2
  </TD> <TD class=c4> 
@@ -131,14 +132,14 @@ RÖ
  </TD> <TD class=c4> 
 
 </TD> </TR>
-<P CLASS="pagebreak"> <TABLE border=1 width=90%> <TR> <TD colspan=4 bgcolor=LightBlue > Vecka 16 - Kapitel 7 forts </TD></TR>
+<TR> <TD colspan=4 bgcolor=LightBlue > Vecka 16 - Kapitel 7 forts </TD></TR>
 <TR> <TD class=c1> 2017-04-21 </TD><TD class=c2> FÖ 10:00   </TD><TD class=c3><A HREF="FL-12"></A>  
 7.7
  </TD> <TD class=c4> 
 
 </TD> </TR>
 
-<P CLASS="pagebreak"> <TABLE border=1 width=90%> <TR> <TD colspan=4 bgcolor=LightBlue > Vecka 17 - Kapitel 7 forts </TD></TR>
+<TR> <TD colspan=4 bgcolor=LightBlue > Vecka 17 - Kapitel 7 forts </TD></TR>
 <TR> <TD class=c1> 2017-04-25 </TD><TD class=c2> FÖ 10:00   </TD><TD class=c3><A HREF="FL-13"></A>  
 7.8
  </TD> <TD class=c4> 
@@ -163,7 +164,7 @@ RÖ
 </TD> </TR>
 
 
-<P CLASS="pagebreak"> <TABLE border=1 width=90%> <TR> <TD colspan=4 bgcolor=LightBlue > Vecka 18 - Kapitel 8  - vibrationer  och </TD></TR>
+<TR> <TD colspan=4 bgcolor=LightBlue > Vecka 18 - Kapitel 8  - vibrationer  och </TD></TR>
 <TR> <TD class=c1> 2017-05-02 </TD><TD class=c2> FÖ 10:00   </TD><TD class=c3><A HREF="FL-16"></A>  
 8.1,8.2
  </TD> <TD class=c4> 
@@ -184,7 +185,7 @@ RÖ
 </TD> </TR>
 
 
-<P CLASS="pagebreak"> <TABLE border=1 width=90%> <TR> <TD colspan=4 bgcolor=LightBlue > Vecka 19 - <A HREF="./kompendium.pdf"> Analytisk mekanik: Kompendium finns att ladda ner</A>   </TD></TR>
+<TR> <TD colspan=4 bgcolor=LightBlue > Vecka 19 - <A HREF="./kompendium.pdf"> Analytisk mekanik: Kompendium finns att ladda ner</A>   </TD></TR>
 <TR> <TD class=c1> 2017-05-09 </TD><TD class=c2> FÖ 10:00   </TD><TD class=c3><A HREF="FL-19"></A>  
 Kapitel 1 -  Introduktion, Kapitel 2-  Generaliserade koordinat
  </TD> <TD class=c4> 
@@ -209,7 +210,7 @@ Kapitel 4 -  Kinetisk energi och generaliserade impulser
 </TD> </TR>
 
 
-<P CLASS="pagebreak"> <TABLE border=1 width=90%> <TR> <TD colspan=4 bgcolor=LightBlue > Vecka 20 - Analytisk mekanik </TD></TR>
+<TR> <TD colspan=4 bgcolor=LightBlue > Vecka 20 - Analytisk mekanik </TD></TR>
 <TR> <TD class=c1> 2017-05-16 </TD><TD class=c2> FÖ 10:00   </TD><TD class=c3><A HREF="FL-22"></A>  
 Kapitel 5 -  LaGrange Ekvationer
  </TD> <TD class=c4> 
@@ -233,7 +234,7 @@ Kapitel 8 -  System med tvång
 </TD> </TR>
 
 
-<P CLASS="pagebreak"> <TABLE border=1 width=90%> <TR> <TD colspan=4 bgcolor=LightBlue > Vecka 21 - Repetition </TD></TR>
+<TR> <TD colspan=4 bgcolor=LightBlue > Vecka 21 - Repetition </TD></TR>
 <TR> <TD class=c1> 2017-05-23 </TD><TD class=c2> FÖ 10:00   </TD><TD class=c3><A HREF="FL-25"></A>  
 Sammanfattning
  </TD> <TD class=c4> 
