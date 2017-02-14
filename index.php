bc3e78e499c28e71034ff0face5d67a50d9d8330
Stellan Ostlund <stellan.ostlund@gmail.com>
2017-02-14 23:27:44 +0100

    finished getting questions that overlap 7th and 8th edition

--- index.php
+++ index.php
@@ -53,7 +53,7 @@ Meriam och Kraige, <em> Dynamics, 8'th Ed </em>
 <br>
 
 
-<em>  Det går bra med andra upplagor; 7'th ed ska gå alldeles utmärkt. Numreringen av uppgifter kan dock avvika från de angivna,
+<em>  Det går bra med andra upplagor; 7'th ed ska gå alldeles utmärkt. Numreringen av uppgifter kan dock avvika från de angivna
 som utgår från 8'e upplagan.  </FONT> </em> 
 <p> <font size="3"><b> Web page:  </b><br><br>
 <A HREF="http://fy.chalmers.se/~ostlund/mek2"> http://fy.chalmers.se/~ostlund/mek2 </A> <br>
@@ -68,35 +68,43 @@ Nedan finns ett <em> inofficiellt schema </em>
 
 
 <TABLE border=1>
-<TR> <TD colspan=4 bgcolor=LightBlue > Vecka 12  Kapitel 5 - Beskrivning av rörelse i två dimensioner </TD></TR>
+<TR> <TD colspan=4 bgcolor=LightBlue > Vecka 12  Kapitel 5 - Beskrivning av rörelse i två dimensioner 
+<A HREF="2017/Kap5"> Uppgifter </A> 
+</TD></TR>
 <TR> <TD class=c1> 2017-03-21 </TD><TD class=c2> FÖ 10:00  </TD><TD class=c3><A HREF="FL-01">5.1,5.2, 5.3  </A>  </TD> <TD class=c4> 
-	uppgift 5.30, (se 5.36 ), 5.51
+	5.2, 5.6, 5.17, 5.27, 5.36,5.49, 5.54
 	</TD> </TR>
 <TR> <TD class=c1> 2017-03-21 </TD><TD class=c2> FÖ 13:15 </TD><TD class=c3><A HREF="FL-02">
 	5.4, 5.5, 5.6 </A> </TD> 
 	<TD class =c4> 
-	5.71,5.76, 5.162, 5.170, 5.173 + koriolis uppgift
+	5.63, 5.68, 5.74, 5.78, 5.103, 5.111, 5.113
 	</TD></TR>
 <TR> <TD class=c1> 2017-03-22 </TD><TD class=c2> FÖ 08:00 </TD><TD class=c3><A HREF="FL-03"> forts </A>  
 	5.7,5.7, 5.8
  </TD> <TD class=c4> 
+	5.122, 5.128, 5.131, 5.143, 5.157, 5.162, 5.177, 5.181, 5.182
 
 </TD> </TR>
 <TR> <TD class=c1> 2017-03-22 </TD><TD class=c2> RÖ 10:00 </TD><TD class=c3><A HREF="LRO.html"></A> </TD><TD>
 RÖ 5.8
 </TD> </TR>
-<TR> <TD colspan=4 bgcolor=LightBlue > Vecka 13  - Kapitel 6 - 2 dimensionell kinematik </TD></TR>
+<TR> <TD colspan=4 bgcolor=LightBlue > Vecka 13  - Kapitel 6 - 2 dimensionell kinematik 
+<A HREF="2017/Kap6"> Uppgifter </A> 
+</TD></TR>
 <TR> <TD class=c1> 2017-03-28 </TD><TD class=c2> RÖ 10:00   </TD><TD class=c3><A HREF="LRO.html"></A> </TD><TD>
 RÖ  - Kapitel 5
 </TD> </TR>
 <TR> <TD class=c1> 2017-03-28 </TD><TD class=c2> FÖ 13:15   </TD><TD class=c3><A HREF="FL-04">
 Kraft, massa och acceleration: 6.1, 6.2,6.3, 6.4, 6.5 </A>
  </TD> <TD class=c4> 
+6.7, 6.19, 6.23,6.27, 6.34, 6.58, 6.74, 6.79, 6.80
+
 
 </TD> </TR>
 <TR> <TD class=c1> 2017-03-29 </TD><TD class=c2> FÖ 08:00   </TD><TD class=c3><A HREF="FL-05">
 Arbete och energi: 6.6, 6.7 </A>
  </TD> <TD class=c4> 
+6.112
 
 </TD> </TR>
 <TR> <TD class=c1> 2017-03-29 </TD><TD class=c2> RÖ 10:00   </TD><TD class=c3><A HREF="LRO.html"></A> </TD><TD>
@@ -105,23 +113,30 @@ RÖ
 <TR> <TD class=c1> 2017-03-31 </TD><TD class=c2> FÖ 10:00   </TD><TD class=c3><A HREF="FL-07">
 Rörelsemängd och Rörelsemängdsmoment: 6.8,6.9 </A>
  </TD> <TD class=c4> 
+6.169, 6.174, 6.181, 6.191, 6.192, 6.202, 6.203
 
 </TD> </TR>
-<TR> <TD colspan=4 bgcolor=LightBlue > Vecka 14  - Kapitel 7 - 3D rotationer </TD></TR>
+<TR> <TD colspan=4 bgcolor=LightBlue > Vecka 14  - Kapitel 7 - 3D rotationer 
+<A HREF="2017/Kap7"> Uppgifter </A> 
+
+</TD></TR>
 <TR> <TD class=c1> 2017-04-04 </TD><TD class=c2> FÖ 10:00   </TD><TD class=c3><A HREF="FL-08"></A>  
 7.1,7.2
  </TD> <TD class=c4> 
+7.1, 7.9
 
 </TD> </TR>
 <TR> <TD class=c1> 2017-04-04 </TD><TD class=c2> FÖ 13:16   </TD><TD class=c3><A HREF="FL-09"></A>  
 
 7.3,7.4
  </TD> <TD class=c4> 
+7.10, 7.14
 
 </TD> </TR>
 <TR> <TD class=c1> 2017-04-05 </TD><TD class=c2> FÖ 08:00   </TD><TD class=c3><A HREF="FL-10"></A>  
 7.5
  </TD> <TD class=c4> 
+ 7.18, 7.26
 
 </TD> </TR>
 <TR> <TD class=c1> 2017-04-05 </TD><TD class=c2> RÖ 10:00   </TD><TD class=c3><A HREF="LRO.html"></A> </TD><TD>
@@ -130,19 +145,27 @@ RÖ
 <TR> <TD class=c1> 2017-04-07 </TD><TD class=c2> FÖ 10:00   </TD><TD class=c3><A HREF="FL-11"></A>  
 7.6
  </TD> <TD class=c4> 
+7.31, 7.37, 7.47
 
 </TD> </TR>
-<TR> <TD colspan=4 bgcolor=LightBlue > Vecka 16 - Kapitel 7 forts </TD></TR>
+<TR> <TD colspan=4 bgcolor=LightBlue > Vecka 16 - Kapitel 7 forts 
+<A HREF="2017/Kap7"> Uppgifter </A> 
+
+</TD></TR>
 <TR> <TD class=c1> 2017-04-21 </TD><TD class=c2> FÖ 10:00   </TD><TD class=c3><A HREF="FL-12"></A>  
 7.7
  </TD> <TD class=c4> 
+7.53,  7.58, 7.61, 7.71, 7.72
 
 </TD> </TR>
 
-<TR> <TD colspan=4 bgcolor=LightBlue > Vecka 17 - Kapitel 7 forts </TD></TR>
+<TR> <TD colspan=4 bgcolor=LightBlue > Vecka 17 - Kapitel 7 forts 
+<A HREF="2017/Kap7"> Uppgifter </A> 
+</TD></TR>
 <TR> <TD class=c1> 2017-04-25 </TD><TD class=c2> FÖ 10:00   </TD><TD class=c3><A HREF="FL-13"></A>  
 7.8
  </TD> <TD class=c4> 
+7.75, 7.78, 7.81, 7.86, 7.91
 
 </TD> </TR>
 <TR> <TD class=c1> 2017-04-25 </TD><TD class=c2> RÖ 13:00   </TD><TD class=c3><A HREF="LRO.html"></A> </TD><TD>
@@ -151,33 +174,42 @@ RÖ
 <TR> <TD class=c1> 2017-04-26 </TD><TD class=c2> FÖ 08:00   </TD><TD class=c3><A HREF="FL-14"></A>  
 7.9,7.10
  </TD> <TD class=c4> 
+7.95, 7.96, 7.99, 7.105
 
 </TD> </TR>
 <TR> <TD class=c1> 2017-04-26 </TD><TD class=c2> RÖ 10:00   </TD><TD class=c3><A HREF="LRO.html"></A> </TD><TD>
 RÖ
 </TD> </TR>
 <TR> <TD class=c1> 2017-04-28 </TD><TD class=c2> FÖ 10:00   </TD><TD class=c3><A HREF="FL-15"></A>  
-7.11,7.12
+7.111, 7.113, 7.11,7.12
 
  </TD> <TD class=c4> 
+7.120, 7.129
 
 </TD> </TR>
 
 
-<TR> <TD colspan=4 bgcolor=LightBlue > Vecka 18 - Kapitel 8  - vibrationer  och </TD></TR>
+<TR> <TD colspan=4 bgcolor=LightBlue > Vecka 18 - Kapitel 8  - vibrationer  
+<A HREF="2017/Kap8"> Uppgifter </A> 
+</TD></TR>
 <TR> <TD class=c1> 2017-05-02 </TD><TD class=c2> FÖ 10:00   </TD><TD class=c3><A HREF="FL-16"></A>  
 8.1,8.2
  </TD> <TD class=c4> 
+8.8, 8.18, 8.23, 8.29, 8.31,8.38, 8.42
 
 </TD> </TR>
 <TR> <TD class=c1> 2017-05-02 </TD><TD class=c2> FÖ 13:00   </TD><TD class=c3><A HREF="FL-17"></A>  
 8.3,8.4
  </TD> <TD class=c4> 
+8.60, 8.62, 8.64, 8.72, 8.73, 8.93, 8.94
+
+
 
 </TD> </TR>
 <TR> <TD class=c1> 2017-05-03 </TD><TD class=c2> FÖ 08:00   </TD><TD class=c3><A HREF="FL-18"></A>  
 8.5,8.6
  </TD> <TD class=c4> 
+8.102, 8.105
 
 </TD> </TR>
 <TR> <TD class=c1> 2017-05-03 </TD><TD class=c2> RÖ 10:00   </TD><TD class=c3><A HREF="LRO.html"></A> </TD><TD>
@@ -185,10 +217,15 @@ RÖ
 </TD> </TR>
 
 
-<TR> <TD colspan=4 bgcolor=LightBlue > Vecka 19 - <A HREF="./kompendium.pdf"> Analytisk mekanik: Kompendium finns att ladda ner</A>   </TD></TR>
+<TR> <TD colspan=4 bgcolor=LightBlue > Vecka 19 - <A HREF="./kompendium.pdf"> Analytisk mekanik: Kompendium finns att ladda ner</A>   
+<A HREF="2017/AnalytiskMekanik"> Uppgifter </A> 
+
+</TD></TR>
 <TR> <TD class=c1> 2017-05-09 </TD><TD class=c2> FÖ 10:00   </TD><TD class=c3><A HREF="FL-19"></A>  
 Kapitel 1 -  Introduktion, Kapitel 2-  Generaliserade koordinat
  </TD> <TD class=c4> 
+1, 4, 7
+
 
 </TD> </TR>
 <TR> <TD class=c1> 2017-05-09 </TD><TD class=c2> RÖ 13:00   </TD><TD class=c3><A HREF="LRO.html"></A> </TD><TD>
@@ -198,7 +235,7 @@ RÖ
 <TR> <TD class=c1> 2017-05-10 </TD><TD class=c2> FÖ 08:00   </TD><TD class=c3><A HREF="FL-20"></A>  
 Kapitel 3 -  Generaliserade krafter
  </TD> <TD class=c4> 
-
+9,12,16
 </TD> </TR>
 <TR> <TD class=c1> 2017-05-10 </TD><TD class=c2> RÖ 10:00   </TD><TD class=c3><A HREF="LRO.html"></A> </TD><TD>
 RÖ
@@ -206,6 +243,7 @@ RÖ
 <TR> <TD class=c1> 2017-05-12 </TD><TD class=c2> FÖ 10:00   </TD><TD class=c3><A HREF="FL-21"></A>  
 Kapitel 4 -  Kinetisk energi och generaliserade impulser
  </TD> <TD class=c4> 
+18, 21
 
 </TD> </TR>
 
@@ -214,6 +252,7 @@ Kapitel 4 -  Kinetisk energi och generaliserade impulser
 <TR> <TD class=c1> 2017-05-16 </TD><TD class=c2> FÖ 10:00   </TD><TD class=c3><A HREF="FL-22"></A>  
 Kapitel 5 -  LaGrange Ekvationer
  </TD> <TD class=c4> 
+22, 25, 31, 34
 
 </TD> </TR>
 <TR> <TD class=c1> 2017-05-16 </TD><TD class=c2> RÖ 13:00   </TD><TD class=c3><A HREF="LRO.html"></A> </TD><TD>
@@ -222,6 +261,7 @@ RÖ
 <TR> <TD class=c1> 2017-05-17 </TD><TD class=c2> FÖ 08:00   </TD><TD class=c3><A HREF="FL-23"></A>  
 Kapitel 6 -  Action principen och  Kapitel 7 -  Hamiltons ekvationer
  </TD> <TD class=c4> 
+41, 46, 54, 60
 
 </TD> </TR>
 <TR> <TD class=c1> 2017-05-17 </TD><TD class=c2> RÖ 10:00   </TD><TD class=c3><A HREF="LRO.html"></A> </TD><TD>
@@ -230,6 +270,7 @@ RÖ
 <TR> <TD class=c1> 2017-05-19 </TD><TD class=c2> FÖ 10:00   </TD><TD class=c3><A HREF="FL-24"></A>  
 Kapitel 8 -  System med tvång
  </TD> <TD class=c4> 
+71, 72
 
 </TD> </TR>
 
